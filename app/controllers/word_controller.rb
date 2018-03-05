1ba1c94fdface5a99a9d900a90ef644527c23d3a
agryaznov <adgryaznov@gmail.com>
2018-03-06 01:39:42 +0300

    hundred words logic

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -1,5 +1,7 @@
 class WordController < ApplicationController
   def quiz
+    @hundreds = Word.count / 100 + 1
+    @checked = params[:hundred] || 0
     if request.post?
       @word = Word.find(params[:word_id])
 
@@ -9,7 +11,8 @@ class WordController < ApplicationController
         if @word.check(params[:guess])
           @word.log(:success)
           flash[:success] = 'Right' # quite right!
-          @word = Word.order("RANDOM()").first
+          # @word = Word.order("RANDOM()").first
+          @word = Word.first(100*(@checked.to_i+1)).last(100).sample
         else
           @word.log(:fail)
           flash[:danger] = 'Nope' # Not quite right!
@@ -20,8 +23,8 @@ class WordController < ApplicationController
       # @word = Word.order("RANDOM()").first
       # take the less tried word
 
-      a = Word.all.sort {|x,y| x.tries.size <=> y.tries.size}
-      @word = a.first
+      # a = Word.all.sort {|x,y| x.tries.size <=> y.tries.size}
+      @word = Word.first(100*(@checked.to_i+1)).last(100).sample
     end
 
     respond_to do |format|
